1775f7d9942260d5e37228811e3a1ea0767f5d4d
HermannW <HermannW@Noose-1099.oosede.local>
2017-06-13 09:56:10 +0200

    initial

---
+++ src/main/java/de/woock/ddd/stattauto/admin/mim/DddStattAutoAdminMimApplication.java
@@ -0,0 +1,14 @@
+package de.woock.ddd.stattauto.admin.mim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DddStattAutoAdminMimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddStattAutoAdminMimApplication.class, args);
+	}
+}
